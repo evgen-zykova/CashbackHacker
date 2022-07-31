83ff477b33903c9c38be7f55a3a1761c36ff6c90
Евгения Пискарева <evgen-zykova@mail.ru>
2022-08-01 00:01:48 +0300

    Add test

---
+++ src/test/java/CashbackHackerServiceTest.java
@@ -0,0 +1,43 @@
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackerService;
+import static org.testng.Assert.*;
+
+public class CashbackHackerServiceTest {
+    CashbackHackerService service = new CashbackHackerService();
+
+    @Test
+    public void calculateNotEnoughFullAmount() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void calculatePurchasePer1000() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculatePurchasePer0() {
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+
+
+
+
+
+
+
+}
+
+
